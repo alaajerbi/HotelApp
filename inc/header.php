dfc3ca3f4b874a1f022d1a462fba3defc7a48af3
AlaaJ <mralaajerbi@gmail.com>
2018-04-11 23:20:29 +0100

    Added authentication check Imported the fontawesome icons Updated the dashboard page

--- inc/header.php
+++ inc/header.php
@@ -1,3 +1,10 @@
+<?php
+    
+    //Check if the user is not logged in and redirect him to the login page.
+    if (!isset($_SESSION['id'])){
+        header('Location:login.php');
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +12,8 @@
     <title><?php echo $pageTitle; ?></title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/styles.css">
-    
+
+    <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -23,7 +31,7 @@
                 <li class="nav-item"><a class="nav-link" href="#">Reservations <img class="nav-img" src="./images/reservations.png"/></a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Customers <img class="nav-img" src="./images/customers.png"/></a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Supplies <img class="nav-img" src="./images/supplies.png"/></a></li>
-
+                <li class="nav-item"><a href="logout.php"><button class="btn btn-primary">Logout <i class="fas fa-sign-out-alt"></i></button></a></li>
             </ul>
         </div>
         <ul class="navbar-nav">
