9cd92bb595f40a4d57da6002ba920bce9f3f349b
Ghammam Anwar <anwarghammam582@gmail.com>
2018-04-20 23:20:19 +0100

    cette page permet de récuperer les données de la reservation

---
+++ save_reservations.php
@@ -0,0 +1,17 @@
+<?php
+$id =$_POST['id'] ;
+$id =$_POST['nom'] ;
+$id =$_POST['prenom'] ;
+$id =$_POST['check_in'] ;
+$id =$_POST['check_out'] ;
+$id =$_POST['num_chambre'] ;
+require_once("conn.php") ;
+$ps=$pdo->prepare("INSERT INTO reservations(nom,prenom,check_in,check_out,num_chambre ) values (?,?,?,?,?)")  ;
+$params=array($nom,$prenom,$check_in,$check_out,$num_chambre) ;
+$ps->execute($params) ;
+header("location:affiche_reservation.php") ;
+$ps=$p
+
+
+
+?>
