5c52527d0ab0d722c31bf895e64b9613b5f2de16
Ghammam Anwar <anwarghammam582@gmail.com>
2018-05-04 16:27:09 +0100

    cette page permet l'authentification de l'agent

--- login.php
+++ login.php
@@ -64,7 +64,7 @@
 
                 }
 
-                else if(data=='ichraf')
+                else if(data=='ok')
                 {
                     swal({
                             title: "Wrong!",
