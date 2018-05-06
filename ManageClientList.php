3c55476ec1cc680e0ab55b09f7fc3e9d67ade2fd
YosraChf <35781220+YosraChf@users.noreply.github.com>
2018-05-06 12:35:32 +0100

    Update ManageClientList.php

--- ManageClientList.php
+++ ManageClientList.php
@@ -30,7 +30,7 @@ $req=$db->query('SELECT nom from client ORDER BY nom ');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styleClientList.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
