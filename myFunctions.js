f2e05c4912d4d7b3353e81ab83cd5c8d36acefa6
ichraf <ichraf7@users.noreply.github.com>
2018-05-08 10:35:53 +0100

    Add files via upload

---
+++ myFunctions.js
@@ -0,0 +1,16 @@
+function room(paramet) {
+
+    swal("Available", 'there are empty rooms', "success")
+
+}
+
+function change(paramet) {
+paramet.setAttribute("value","yes");
+}
+/*
+function afficher(argu) {
+    if(argu.hasAttribute("value"))
+    {
+     var n =$(ar)
+    }
+}*/
