e8d27cc431151b6dc13ce1fc6088343404e5c7a0
ichraf <ichraf7@users.noreply.github.com>
2018-05-09 18:19:31 +0100

    Update database_connexion.php

--- database_connexion.php
+++ database_connexion.php
@@ -1,6 +1,8 @@
 
 <?php
-
+if (!isset($_SESSION['id'])) {
+        header('Location: login.php');
+    }
 try
 {
     $bdd = new PDO('mysql:host=localhost;dbname=db_hotel;charset=utf8', 'root', '');
