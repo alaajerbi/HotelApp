d93517251135d62a6c854e0080ddf48664d0c191
ichraf <ichraf7@users.noreply.github.com>
2018-05-09 18:16:34 +0100

    search empty room

---
+++ searchRoom.php
@@ -0,0 +1,18 @@
+
+<?php
+if (isset($_POST['room'])) {
+    include 'database_connexion.php';
+
+    $req = $bdd->prepare("SELECT num_chambre FROM `chambre` WHERE disponiblite=:disponib AND type=:types");
+    $var1 = 'dispo';
+    $var2 =$_POST['room'];
+    $req->execute(array('disponib' => $var1, 'types' => $var2));
+
+
+    if ($res = $req->fetch()) {
+        echo $res['num_chambre'];
+    }
+
+}
+
+        ?>
