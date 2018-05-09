37d5d7a1291dc4b24e31cdb46636bedadeee9cea
ichraf <ichraf7@users.noreply.github.com>
2018-05-09 18:22:35 +0100

    Update myFunctions.js

--- myFunctions.js
+++ myFunctions.js
@@ -1,16 +1,4 @@
-function room(paramet) {
-
-    swal("Available", 'there are empty rooms', "success")
-
-}
 
 function change(paramet) {
 paramet.setAttribute("value","yes");
 }
-/*
-function afficher(argu) {
-    if(argu.hasAttribute("value"))
-    {
-     var n =$(ar)
-    }
-}*/
